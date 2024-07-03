de228483b0b6b749f4591fcd83e2d10cd1ea62b8
MattiaBologna <mattiabologna01@gmail.com>
2024-07-03 11:15:51 +0200

    Add: fetchApartments() appcontent

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,11 +1,24 @@
 <script>
+import axios from 'axios'
 
 export default {
   components: {
   },
   data() {
     return {
+      apartments: []
     }
+  },
+  methods: {
+    fetchApartments() {
+      axios.get('http://127.0.0.1:8000/api/apartments').then(res => {
+        this.apartments = res.data
+        console.log(this.apartments)
+      })
+    }
+  },
+  created() {
+    this.fetchApartments()
   }
 }
 
@@ -19,6 +32,7 @@ export default {
         <div class="row aling-items-center flex-column justify-content-center h-100">
           <div class="col-auto text-center">
             <h1>Hello Vue <font-awesome-icon :icon="['fab', 'vuejs']" /></h1>
+            <p v-for="apartment in apartments">{{ apartment.title }}</p>
           </div>
         </div>
       </div>
