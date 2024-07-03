ac73880dc543050d0acb758a4eaa658acce0f452
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-07-03 17:28:46 +0200

    Added: structure research page

--- src/pages/Research.vue
+++ src/pages/Research.vue
@@ -1,7 +1,59 @@
 <template>
-    <div>
+    <section>
+        <div class="page-research-input">
+            <form class="flex-form" action="">
+                <div>
+                    <label for="input">Address:</label>
+                    <input type="text" id="input">
+                </div>
+                <button><font-awesome-icon :icon="['fas', 'magnifying-glass']" /></button>
+            </form>
+        </div>
+    </section>
 
-    </div>
+    <section>
+        <div class="filter-container">
+            <span><font-awesome-icon :icon="['fas', 'filter']" /></span>
+            <p>More filters</p>
+            <span><font-awesome-icon :icon="['fas', 'sort-down']" /></span>
+        </div>
+    </section>
+
+    <section>
+        <div class="research-row-articles">
+            <article class="article-research">
+                <div class="col-img">
+                    <img src="" alt="">
+                    <span>12,3 Km</span>
+                </div>
+                <div class="col-info">
+                    <p>House for the perfect group vacation</p>
+                    <div class="research-container-info">
+                        <div class="research-article-info">
+                            <img class="icon-info" src="/public/img/icon_room_01.png" alt="">
+                            <span>Rooms</span>
+                            <span>6</span>
+                        </div>
+                        <div>
+                            <img class="icon-info" src="/public/img/icon_space_01.png" alt="">
+                            <span>m ^2</span>
+                            <span>173</span>
+                        </div>
+                        <div>
+                            <img class="icon-info" src="/public/img/icon_room_01.png" alt="">
+                            <span>Beds</span>
+                            <span>4</span>
+                        </div>
+                        <div>
+                            <img class="icon-info" src="/public/img/icon_bathroom_01.png" alt="">
+                            <span>Bathroom</span>
+                            <span>2</span>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </section>
 </template>
 
 <script>
