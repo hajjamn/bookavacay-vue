f173576ab460b03022b435ff00139b027d59f634
hajjamn <hajjamn@gmail.com>
2024-07-03 15:32:16 +0200

    ADD: some shenanigans

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,182 +1,3 @@
-<template>
-    <main>
-        <section class="section-title">
-            <div class="container-title">
-                <div>
-                    <p>The best way</p>
-                    <p class="to-book">to book your next vacay</p>
-                </div>
-            </div>
-        </section>
-        <section>
-            <div class="container-article">
-                <h1>Our top choices:</h1>
-                <div class="row-article">
-                    <article class="col-article">
-                        <img src="https://picsum.photos/seed/picsum/300/200" alt="">
-                        <p>Casa mia</p>
-                        <div class="container-article-info">
-                            <div class="article-info primo">
-                                <img src="/public/img/icon_room_01.png" alt="">
-                                <span>Rooms</span>
-                                <span>12</span>
-                            </div>
-                            <div class="article-info secondo">
-                                <img src="/public/img/icon_space_01.png" alt="">
-                                <span>m ^2</span>
-                                <span>286</span>
-                            </div>
-                            <div class="article-info terzo">
-                                <img src="/public/img/icon_room_01.png" alt="">
-                                <span>Beds</span>
-                                <span>8</span>
-                            </div>
-                            <div class="article-info">
-                                <img src="/public/img/icon_bathroom_01.png" alt="">
-                                <span>Bathroom</span>
-                                <span>6</span>
-                            </div>
-                        </div>
-                    </article>
-                    <article class="col-article">
-                        <img src="https://picsum.photos/seed/picsum/300/200" alt="">
-                        <p>House for the perfect group vacation</p>
-                        <div class="container-article-info">
-                            <div class="article-info primo">
-                                <img src="/public/img/icon_room_01.png" alt="">
-                                <span>Rooms</span>
-                                <span>12</span>
-                            </div>
-                            <div class="article-info secondo">
-                                <img src="/public/img/icon_space_01.png" alt="">
-                                <span>m ^2</span>
-                                <span>286</span>
-                            </div>
-                            <div class="article-info terzo">
-                                <img src="/public/img/icon_room_01.png" alt="">
-                                <span>Beds</span>
-                                <span>8</span>
-                            </div>
-                            <div class="article-info">
-                                <img src="/public/img/icon_bathroom_01.png" alt="">
-                                <span>Bathroom</span>
-                                <span>6</span>
-                            </div>
-                        </div>
-                    </article>
-                    <article class="col-article">
-                        <img src="https://picsum.photos/seed/picsum/300/200" alt="">
-                        <p>House for the perfect group vacation</p>
-                        <div class="container-article-info">
-                            <div class="article-info primo">
-                                <img src="/public/img/icon_room_01.png" alt="">
-                                <span>Rooms</span>
-                                <span>12</span>
-                            </div>
-                            <div class="article-info secondo">
-                                <img src="/public/img/icon_space_01.png" alt="">
-                                <span>m ^2</span>
-                                <span>286</span>
-                            </div>
-                            <div class="article-info terzo">
-                                <img src="/public/img/icon_room_01.png" alt="">
-                                <span>Beds</span>
-                                <span>8</span>
-                            </div>
-                            <div class="article-info">
-                                <img src="/public/img/icon_bathroom_01.png" alt="">
-                                <span>Bathroom</span>
-                                <span>6</span>
-                            </div>
-                        </div>
-                    </article>
-                    <article class="col-article">
-                        <img src="https://picsum.photos/seed/picsum/300/200" alt="">
-                        <p>House for the perfect group vacation</p>
-                        <div class="container-article-info">
-                            <div class="article-info primo">
-                                <img src="/public/img/icon_room_01.png" alt="">
-                                <span>Rooms</span>
-                                <span>12</span>
-                            </div>
-                            <div class="article-info secondo">
-                                <img src="/public/img/icon_space_01.png" alt="">
-                                <span>m ^2</span>
-                                <span>286</span>
-                            </div>
-                            <div class="article-info terzo">
-                                <img src="/public/img/icon_room_01.png" alt="">
-                                <span>Beds</span>
-                                <span>8</span>
-                            </div>
-                            <div class="article-info">
-                                <img src="/public/img/icon_bathroom_01.png" alt="">
-                                <span>Bathroom</span>
-                                <span>6</span>
-                            </div>
-                        </div>
-                    </article>
-                    <article class="col-article">
-                        <img src="https://picsum.photos/seed/picsum/300/200" alt="">
-                        <p>House for the perfect group vacation</p>
-                        <div class="container-article-info">
-                            <div class="article-info primo">
-                                <img src="/public/img/icon_room_01.png" alt="">
-                                <span>Rooms</span>
-                                <span>12</span>
-                            </div>
-                            <div class="article-info secondo">
-                                <img src="/public/img/icon_space_01.png" alt="">
-                                <span>m ^2</span>
-                                <span>286</span>
-                            </div>
-                            <div class="article-info terzo">
-                                <img src="/public/img/icon_room_01.png" alt="">
-                                <span>Beds</span>
-                                <span>8</span>
-                            </div>
-                            <div class="article-info">
-                                <img src="/public/img/icon_bathroom_01.png" alt="">
-                                <span>Bathroom</span>
-                                <span>6</span>
-                            </div>
-                        </div>
-                    </article>
-                    <article class="col-article">
-                        <img src="https://picsum.photos/seed/picsum/300/200" alt="">
-                        <p>House for the perfect group vacation</p>
-                        <div class="container-article-info">
-                            <div class="article-info primo">
-                                <img src="/public/img/icon_room_01.png" alt="">
-                                <span>Rooms</span>
-                                <span>12</span>
-                            </div>
-                            <div class="article-info secondo">
-                                <img src="/public/img/icon_space_01.png" alt="">
-                                <span>m ^2</span>
-                                <span>286</span>
-                            </div>
-                            <div class="article-info terzo">
-                                <img src="/public/img/icon_room_01.png" alt="">
-                                <span>Beds</span>
-                                <span>8</span>
-                            </div>
-                            <div class="article-info">
-                                <img src="/public/img/icon_bathroom_01.png" alt="">
-                                <span>Bathroom</span>
-                                <span>6</span>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-            </div>
-        </section>
-    </main>
-    <!-- <img class="svg-wave" src="/public/img/wave.svg" alt=""> -->
-
-</template>
-
-
 <script>
 import axios from 'axios'
 
@@ -188,18 +9,91 @@ export default {
         }
     },
     methods: {
-        fetchApartments() {
-            axios.get('http://127.0.0.1:8000/api/apartments').then(res => {
-                this.apartments = res.data
-                console.log(this.apartments)
+        fetchApartments(perPage) {
+            axios.get('http://127.0.0.1:8000/api/apartments', {
+                params: {
+                    perPage
+                }
             })
+                .then(res => {
+                    console.log(res)
+                    this.apartments = res.data.apartments.data
+                    console.log(this.apartments)
+                })
         }
     },
     created() {
-        this.fetchApartments()
+        this.fetchApartments(6)
     }
 }
 
 </script>
 
-<style scoped></style>
+<template>
+    <main>
+        <section class="section-title">
+            <div class="container-title">
+                <div>
+                    <p>The best way</p>
+                    <p class="to-book">to book your next vacay</p>
+                </div>
+            </div>
+        </section>
+        <section id="articles">
+            <div class="container-article">
+                <h1>Our top choices:</h1>
+                <div class="row-article">
+                    <article v-for="apartment in apartments" class="col-article">
+                        <img src="https://picsum.photos/seed/picsum/300/200" alt="">
+                        <p>{{ apartment.title }}</p>
+                        <div class="container-article-info">
+                            <div class="article-info primo">
+                                <img src="/public/img/icon_room_01.png" alt="">
+                                <span>Rooms</span>
+                                <span>{{ apartment.rooms }}</span>
+                            </div>
+                            <div class="article-info secondo">
+                                <img src="/public/img/icon_space_01.png" alt="">
+                                <span>m ^2</span>
+                                <span>{{ apartment.sqr_mt }}</span>
+                            </div>
+                            <div class="article-info terzo">
+                                <img src="/public/img/icon_room_01.png" alt="">
+                                <span>Beds</span>
+                                <span>{{ apartment.beds }}</span>
+                            </div>
+                            <div class="article-info">
+                                <img src="/public/img/icon_bathroom_01.png" alt="">
+                                <span>Bathroom</span>
+                                <span>{{ apartment.bathrooms }}</span>
+                            </div>
+                        </div>
+                    </article>
+
+
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- <img class="svg-wave" src="/public/img/wave.svg" alt=""> -->
+    <section class="svg-wave">
+
+    </section>
+
+
+
+</template>
+
+
+
+
+<style scoped>
+.svg-wave {
+    background-image: url('/public/img/wave.svg');
+    background-size: contain;
+    background-repeat: repeat;
+    background-position: bottom left;
+    width: 100%;
+    height: 200px;
+}
+</style>
