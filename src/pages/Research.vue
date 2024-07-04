9846b402c166e8a6b0f91b5798dfff39cb314a50
EnricoClem <enrico.clem94@gmail.com>
2024-07-04 12:31:22 +0200

    ADD: advanced research filter style

--- src/pages/Research.vue
+++ src/pages/Research.vue
@@ -13,59 +13,72 @@
         </div>
     </section>
 
+    <!-- Filtri extra -->
     <section>
-        <div class="filter-container">
-            <span><font-awesome-icon :icon="['fas', 'filter']" /></span>
-            <p>More filters</p>
-            <span><font-awesome-icon :icon="['fas', 'sort-down']" /></span>
+        <!-- filter head -->
+        <div class="filter-box">
+            <div class="filter-container">
+                <img src="/public/img/icon_filter_01.png" alt="">
+                <p>More filters</p>
+                <span><font-awesome-icon :icon="['fas', 'sort-down']" /></span>
+            </div>
         </div>
-    </section>
 
-    <section>
-        <div class="research-row-articles">
-            <article class="article-research">
-                <div class="col-img">
-                    <img src="" alt="">
-                    <span>12,3 Km</span>
-                </div>
-                <div class="col-info">
-                    <p>House for the perfect group vacation</p>
-                    <div class="research-container-info">
-                        <div class="research-article-info">
-                            <img class="icon-info" src="/public/img/icon_room_01.png" alt="">
-                            <span>Rooms</span>
-                            <span>6</span>
-                        </div>
-                        <div>
-                            <img class="icon-info" src="/public/img/icon_space_01.png" alt="">
-                            <span>m ^2</span>
-                            <span>173</span>
-                        </div>
-                        <div>
-                            <img class="icon-info" src="/public/img/icon_room_01.png" alt="">
-                            <span>Beds</span>
-                            <span>4</span>
-                        </div>
-                        <div>
-                            <img class="icon-info" src="/public/img/icon_bathroom_01.png" alt="">
-                            <span>Bathroom</span>
-                            <span>2</span>
-                        </div>
+        <!-- filter body -->
+        <div class="filter-box">
+            <div class="all-filter-container">
+                <div class="numeric-filters">
+                    <div class="filter-num">
+                        <p>Min beds:</p>
+                        <input class="input-num" type="number" name="" id="" placeholder="1">
+                    </div>
+                    <div class="filter-num">
+                        <p>Min rooms:</p>
+                        <input class="input-num" type="number" name="" id="" placeholder="1">
+                    </div>
+                    <div class="filter-num">
+                        <p>Search km radius:</p>
+                        <input class="input-num" type="number" name="" id="" placeholder="1">
                     </div>
                 </div>
-            </article>
+                <div class="filters-divider"></div>
+
+                <!-- SERVONO SERVICES API -->
+                <!-- <div class="filter-service">
+                    <div v-for="service in services">
+                        <input type="checkbox" name="" id="">
+                        <p>{{ service.name }}</p>
+                    </div>
+                </div> -->
+            </div>
         </div>
     </section>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-        }
-    }
+// import axios from 'axios';
 
-}
+// export default {
+//     data() {
+//         return {
+//             services: []
+//         }
+//     },
+//     created() {
+//         this.fetchServices();
+//     },
+//     methods: {
+//         fetchServices() {
+//             axios.get('/api/services')
+//                 .then(response => {
+//                     this.services = response.data;
+//                 })
+//                 .catch(error => {
+//                     console.error('Error fetching services:', error);
+//                 });
+//         }
+//     }
+// }
 </script>
 
 <style scoped></style>
