cbcc6de6298bfb386268ff5258c34f117e55846a
EnricoClem <enrico.clem94@gmail.com>
2024-07-05 16:02:18 +0200

    ADD: apartment detail info card

--- src/pages/ApartmentDetail.vue
+++ src/pages/ApartmentDetail.vue
@@ -1,5 +1,6 @@
 <script>
 import axios from 'axios';
+import { getImageUrl } from "../functions.js";
 
 export default {
   props: ['id'],
@@ -12,6 +13,7 @@ export default {
     this.fetchApartment();
   },
   methods: {
+    getImageUrl,
     fetchApartment() {
       axios.get(`http://127.0.0.1:8000/api/apartments/${this.id}`)
         .then(res => {
@@ -26,9 +28,47 @@ export default {
 </script>
 
 <template>
-  <div>
-    <h1>Apartment Detail Page</h1>
-    <p>Apartment ID: {{ id }}</p>
-    <p>Title: {{ apartment.title }}</p>
-  </div>
+
+  <section class="container-sm">
+    <!-- CONTAINER CARD APARTMENT -->
+    <div class="apartment-detail-card">
+      <!-- IMMAGINE SINISTRA -->
+      <div>
+        <img class="image-container" :src="getImageUrl(apartment.image)" alt="">
+      </div>
+      <!-- DATI DESTRA -->
+      <div class="data-container">
+        <h1>{{ apartment.title }}</h1>
+        <h4>Indirizzo appartamento</h4>
+        <div class="detail-container">
+          <div class="detail-info">
+              <img class="icon-detail" src="/public/img/icon_room_01.png" alt="">
+              <span>Rooms</span>
+              <span>{{ apartment.rooms }}</span>
+          </div>
+          <div class="icon-divider"></div>
+          <div class="detail-info">
+              <img class="icon-detail" src="/public/img/icon_space_01.png" alt="">
+              <span>m ^2</span>
+              <span>{{ apartment.sqr_mt }}</span>
+          </div>
+          <div class="icon-divider"></div>
+          <div class="detail-info">
+              <img class="icon-detail" src="/public/img/icon_room_01.png" alt="">
+              <span>Beds</span>
+              <span>{{ apartment.beds }}</span>
+          </div>
+          <div class="icon-divider"></div>
+          <div class="detail-info">
+              <img class="icon-detail" src="/public/img/icon_bathroom_01.png" alt="">
+              <span>Bathroom</span>
+              <span>{{ apartment.bathrooms }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
 </template>
+
+<style scoped></style>
