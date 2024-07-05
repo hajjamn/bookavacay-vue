a2b27dfda52687ba011226acc9a9fd3e8803ba0f
EnricoClem <enrico.clem94@gmail.com>
2024-07-05 17:32:16 +0200

    ADD: apartment detail working map and send message style

--- src/pages/ApartmentDetail.vue
+++ src/pages/ApartmentDetail.vue
@@ -6,7 +6,9 @@ export default {
   props: ['id'],
   data() {
     return {
-      apartment: {}
+      apartment: {},
+      latitude: null,
+      longitude: null
     };
   },
   created() {
@@ -18,11 +20,44 @@ export default {
       axios.get(`http://127.0.0.1:8000/api/apartments/${this.id}`)
         .then(res => {
           this.apartment = res.data.apartment;
+          // Imposta le coordinate dell'appartamento
+          this.latitude = this.apartment.latitude;
+          this.longitude = this.apartment.longitude;
+          this.initializeMap();
         })
         .catch(error => {
           console.error('Error fetching apartment:', error);
         });
+    },
+    initializeMap() {
+      console.log("Initializing map..."); // Debug log
+      if (
+        typeof tt !== "undefined" &&
+        typeof tt.map !== "undefined" &&
+        typeof tt.services !== "undefined"
+      ) {
+        let map = tt.map({
+          key: "VtdGJcQDaomboK5S3kbxFvhtbupZjoK0",
+          container: "map",
+          center: [this.longitude, this.latitude],
+          zoom: 15
+        });
+
+        let marker = new tt.Marker({
+          draggable: false, // Marker non trascinabile
+        })
+          .setLngLat([this.longitude, this.latitude])
+          .addTo(map);
+      } else {
+        console.error("TomTom SDK not loaded properly.");
+      }
     }
+  },
+  mounted() {
+    // Rimuovi la chiamata a initializeMap da qui
+    // this.$nextTick(() => {
+    //   this.initializeMap();
+    // });
   }
 };
 </script>
@@ -40,7 +75,7 @@ export default {
       <div class="data-container">
         <div>
           <h1>{{ apartment.title }}</h1>
-          <h4>Indirizzo appartamento</h4>
+          <h4>{{ apartment.address }}</h4>
         </div>
         <div class="detail-container">
           <div class="detail-info">
@@ -70,7 +105,34 @@ export default {
       </div>
     </div>
   </section>
+  
+  <!-- MAPPA -->
+  <section>
+    <div class="container-sm container-map">
+      <div id="search-map">
+        <div id="searchbar"></div>
+        <div id="map"></div>
+      </div>
+    </div>
+  </section>
+
+  <!-- MESSAGGIO -->
+   <section>
+    <form class="container-message">
+      <label class="message-label">
+        <p>Send a message to the owner:</p>
+      </label>
+      <textarea class="message-box" name="" id="" cols="30" rows="5" placeholder="Hi! Is the apartment still available?"></textarea>
+      <div class="message-leg"></div>
+      <div>
+        <button class="message-send">
+          <a href="">Send</a>
+        </button>
+      </div>
+    </form>
+   </section>
 
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
